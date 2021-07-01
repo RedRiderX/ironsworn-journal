45338ad27793b1c8b938f6946cc22847164ceb5b
Red McGreg <contactus@redriderx.com>
2021-07-01 19:00:00 -0400

    flesh out momentum roll ui

--- components/log/ItemRoll.vue
+++ components/log/ItemRoll.vue
@@ -1,80 +1,135 @@
 <template>
-  <LogItem class="log-item--meta" canReroll canDelete :uuid="uuid" @reroll-log="reroll">
+  <LogItem
+    class="log-item--meta"
+    canReroll
+    canDelete
+    :uuid="uuid"
+    @reroll-log="reroll"
+  >
     <div class="log-item__wrapper mx-auto">
       <h1 class="text-xl font-display text-center mb-2">{{ moveTitle }}</h1>
       <div class="dice-roll bg-gray-500 flex justify-around p-2">
         <div class="action-score flex flex-none flex-col items-center">
           <h2 class="text-center font-bold">Action Score</h2>
           <div
-            class="action-score__value bg-gray-100 font-display text-3xl w-16 h-16 m-1 flex items-center justify-center border-gray-800 border-4 rounded-full"
+            class="action-score__value relative bg-gray-100 font-display text-3xl w-16 h-16 m-1 flex items-center justify-center border-gray-800 border-4 rounded-full"
           >
             <span class="leading-none">{{ actionScore }}</span>
+            <div v-show="showNegativeMomentumAlert" class="status status--burn">
+              <BurnIcon class="w-6 h-6 p-1 fill-current text-orange-500" />
+            </div>
           </div>
         </div>
         <div class="challenge-dice flex-none">
           <h2 class="text-center font-bold">Challenge Dice</h2>
           <div class="challenge-dice__rolls flex justify-center mt-1">
             <div
-              class="challenge-dice-roll flex-shrink relative bg-gray-100 font-display text-3xl m-1 flex items-center justify-center border-gray-800 border-4 rounded-lg"
+              class="challenge-dice-roll transform rotate-45 flex-shrink relative bg-gray-100 font-display text-3xl m-1 flex items-center justify-center border-gray-800 border-4 rounded-lg"
             >
-              <span class="challenge-dice-roll__value leading-none">{{ challengeDice[0] }}</span>
+              <span
+                class="challenge-dice-roll__value transform -rotate-45 leading-none"
+                >{{ challengeDice[0] }}</span
+              >
               <div
                 v-show="actionScore > challengeDice[0]"
-                class="status status--hit absolute right-0 bottom-0 bg-gray-800 rounded-full"
+                class="status status--hit"
               >
-                <CheckIcon class="w-6 h-6 fill-current text-green-500"/>
+                <CheckIcon class="w-6 h-6 fill-current text-green-500" />
               </div>
               <div
                 v-show="actionScore <= challengeDice[0]"
-                class="status status--miss absolute right-0 bottom-0 bg-gray-800 rounded-full"
+                class="status status--miss"
               >
-                <CrossIcon class="w-6 h-6 fill-current text-red-600"/>
+                <CrossIcon class="w-6 h-6 fill-current text-red-600" />
+              </div>
+              <div v-show="challengeDice0Burned" class="status status--burn">
+                <BurnIcon class="w-6 h-6 p-1 fill-current text-orange-500" />
               </div>
             </div>
             <div
-              class="challenge-dice-roll flex-shrink relative bg-gray-100 font-display text-3xl m-1 flex items-center justify-center border-gray-800 border-4 rounded-lg"
+              class="challenge-dice-roll transform rotate-45 flex-shrink relative bg-gray-100 font-display text-3xl m-1 flex items-center justify-center border-gray-800 border-4 rounded-lg"
             >
-              <span class="challenge-dice-roll__value leading-none">{{ challengeDice[1] }}</span>
+              <span
+                class="challenge-dice-roll__value transform -rotate-45 leading-none"
+                >{{ challengeDice[1] }}</span
+              >
               <div
                 v-show="actionScore > challengeDice[1]"
-                class="status status--hit absolute right-0 bottom-0 bg-gray-800 rounded-full"
+                class="status status--hit"
               >
-                <CheckIcon class="w-6 h-6 fill-current text-green-500"/>
+                <CheckIcon class="w-6 h-6 fill-current text-green-500" />
               </div>
               <div
                 v-show="actionScore <= challengeDice[1]"
-                class="status status--miss absolute right-0 bottom-0 bg-gray-800 rounded-full"
+                class="status status--miss"
               >
-                <CrossIcon class="w-6 h-6 fill-current text-red-600"/>
+                <CrossIcon class="w-6 h-6 fill-current text-red-600" />
+              </div>
+              <div v-show="challengeDice1Burned" class="status status--burn">
+                <BurnIcon class="w-6 h-6 p-1 fill-current text-orange-500" />
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="dice-roll-result bg-gray-300 text-center uppercase tracking-wide p-1">
+      <div
+        v-if="showNegativeMomentumAlert"
+        class="negative-momentum-alert bg-gray-600 text-white uppercase tracking-wide text-center p-1"
+      >
+        Action Die <strong>Cancelled</strong>
+      </div>
+      <div
+        v-show="challengeDice0Burned || challengeDice1Burned"
+        class="burned-momentum-alert bg-gray-600 text-white uppercase tracking-wide text-center p-1"
+      >
+        Challenge Dice <strong>Cancelled</strong>
+      </div>
+      <div
+        class="dice-roll-result bg-gray-300 text-center uppercase tracking-wide p-1"
+      >
         {{ rollResult }}
-        <span v-if="this.challengeDice[0] === this.challengeDice[1]">(Match)</span>
+        <span v-if="this.challengeDice[0] === this.challengeDice[1]"
+          >(Match)</span
+        >
       </div>
       <div
         v-if="this.challengeDice[0] === this.challengeDice[1]"
         class="move-result mt-3 mx-3 border-b p-2"
       >
-        <template v-if="rollResult === 'Strong Hit' ">
+        <template v-if="rollResult === 'Strong Hit'">
           <p>
-            <strong>Strong hit:</strong> The
-            <strong>match</strong> should represent a twist in the narrative, something
-            interesting, or a new opportunity.
+            <strong>Strong hit:</strong> The <strong>match</strong> should
+            represent a twist in the narrative, something interesting, or a new
+            opportunity.
           </p>
         </template>
         <template v-else>
           <p>
-            <strong>Miss:</strong> The
-            <strong>match</strong> should represent a heightened negative outcome, a
-            complication, or new danger. Things get worse for you in an unexpected way.
+            <strong>Miss:</strong> The <strong>match</strong> should represent a
+            heightened negative outcome, a complication, or new danger. Things
+            get worse for you in an unexpected way.
           </p>
         </template>
       </div>
-      <div v-if="moveResult" class="move-result mt-3 mx-3" v-html="moveResult"></div>
+      <div
+        v-if="moveResult"
+        class="move-result mt-3 mx-3"
+        v-html="moveResult"
+      ></div>
+    </div>
+    <div
+      class="burn-momentum-prompt flex items-center justify-between border border-gray-400 p-1 bg-gray-200 mb-2 mt-4"
+    >
+      <span
+        ><strong>Burn Momentum</strong> for a <strong>Weak Hit</strong>?</span
+      >
+      <button
+        class="bg-gray-600 py-1 px-2 text-white font-bold uppercase text-sm rounded self-center flex items-center"
+        @click="burnMomentum"
+      >
+        <span class="leading-none mx-1 inline-block">Burn</span>
+        <BurnIcon class="w-5 h-5 fill-current" />
+      </button>
     </div>
   </LogItem>
 </template>
@@ -83,6 +138,7 @@
 import LogItem from "~/components/log/BaseItem";
 import CheckIcon from "~/assets/icons/check.svg";
 import CrossIcon from "~/assets/icons/cross.svg";
+import BurnIcon from "~/assets/icons/burn.svg";
 import dieRoll from "~/plugins/dice";
 
 export default {
@@ -90,6 +146,7 @@ export default {
     LogItem,
     CheckIcon,
     CrossIcon,
+    BurnIcon,
   },
   props: {
     uuid: String,
@@ -108,6 +165,9 @@ export default {
         initialState.challengeDice[0],
         initialState.challengeDice[1],
       ],
+      showNegativeMomentumAlert: false,
+      challengeDice0Burned: false,
+      challengeDice1Burned: false,
     };
   },
   computed: {
@@ -163,9 +223,8 @@ export default {
             overcome this obstacle</li>
             <li>You give up: <em>Forsake Your Vow</em>.</li></ul>`;
         }
-      } else {
-        return null;
       }
+      return null;
     },
   },
   created() {
@@ -191,26 +250,26 @@ export default {
         challengeDice: this.challengeDice,
       });
     },
+    burnMomentum() {
+      // do something?
+    },
   },
 };
 </script>
 
-<style scoped>
+<style lang="postcss" scoped>
 .log-item__wrapper {
   width: 18rem;
 }
 .challenge-dice-roll {
-  transform: rotate(45deg);
   width: 3.5rem;
   height: 3.5rem;
 }
-.challenge-dice-roll__value,
 .status {
-  transform: rotate(-45deg);
+  @apply absolute -right-4 bottom-0 bg-gray-800 rounded-full p-px;
 }
-.status {
+.challenge-dice .status {
+  @apply transform -rotate-45;
   bottom: calc(50% - 0.75rem);
-  right: -1rem;
-  padding: 1px;
 }
 </style>
--- pages/about.vue
+++ pages/about.vue
@@ -1,53 +1,53 @@
 <template>
-  <div class="container h-screen flex flex-col items-center justify-center">
-    <h1 class="font-display text-2xl">A Journal for Solo Ironsworn play</h1>
-    <p>Some about text here...</p>
-    <div class="credits max-w-sm mt-2 mb-4">
-      <h2 class="text-xl">Credits</h2>
-      <p class="mb-2">
-        <a href="https://github.com/RedRiderX/ironsworn-journal">Code by Red</a>
-        & licensed under GPL3.
-      </p>
-      <p class="mb-2">
-        <a href="https://game-icons.net/1x1/lorc/treasure-map.html"
-          >Treasure map icon</a
+  <div
+    class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8"
+  >
+    <div class="max-w-prose text-center">
+      <h1 class="font-display text-2xl">A Journal for Solo Ironsworn play</h1>
+      <p>Some about text here...</p>
+      <div class="credits text-left mt-2 mb-4">
+        <h2 class="text-xl">Credits</h2>
+        <p class="mb-2">
+          <a href="https://github.com/RedRiderX/ironsworn-journal"
+            >Code by Red</a
+          >
+          & licensed under GPL3.
+        </p>
+        <p class="mb-2">
+          <a href="https://game-icons.net/1x1/lorc/treasure-map.html"
+            >Treasure map icon</a
+          >
+          by Lorc under
+          <a href="https://creativecommons.org/licenses/by/3.0/">CC BY 3.0</a>.
+        </p>
+        <p class="mb-2">
+          <a href="https://game-icons.net/1x1/sbed/flamer.html">Flamer icon</a>
+          by sbed under
+          <a href="https://creativecommons.org/licenses/by/3.0/">CC BY 3.0</a>.
+        </p>
+        <p class="mb-2">
+          This work is based on
+          <a href="https://www.ironswornrpg.com" target="_blank">Ironsworn</a>,
+          created by Shawn Tomkin, and licensed for our use under the
+          <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/Creative"
+            >Commons Attribution-NonCommercial-ShareAlike 4.0 International
+            license</a
+          >.
+        </p>
+        <p></p>
+      </div>
+      <div>
+        <NLink
+          to="/"
+          class="bg-accent-500 text-white font-bold uppercase text-sm rounded py-2 px-4 tracking-wide"
+          >Back home</NLink
         >
-        by Lorc under
-        <a href="https://creativecommons.org/licenses/by/3.0/">CC BY 3.0</a>.
-      </p>
-      <p class="mb-2">
-        This work is based on
-        <a href="https://www.ironswornrpg.com" target="_blank">Ironsworn</a>,
-        created by Shawn Tomkin, and licensed for our use under the
-        <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/Creative"
-          >Commons Attribution-NonCommercial-ShareAlike 4.0 International
-          license</a
-        >.
-      </p>
-      <p></p>
-    </div>
-    <div>
-      <NLink
-        to="/"
-        class="bg-accent-500 text-white font-bold uppercase text-sm rounded py-2 px-4 tracking-wide"
-        >Back home</NLink
-      >
+      </div>
     </div>
   </div>
 </template>
 
-<script>
-export default {};
-</script>
-
 <style lang="postcss" scoped>
-/* .container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-} */
 .credits a {
   @apply underline;
 }
